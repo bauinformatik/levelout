c9bdbc48329d05950a2f539bd6f5bbf328eacfce
Subha <subhashini.krishnakumar@uni-weimar.de>
2023-01-16 13:46:35 +0100

    Ifclocal to Global Wgs Coord

--- src/main/java/org/opensourcebim/levelout/samples/OsmInteractive.java
+++ src/main/java/org/opensourcebim/levelout/samples/OsmInteractive.java
@@ -4,10 +4,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+
+import org.locationtech.proj4j.CRSFactory;
+import org.locationtech.proj4j.CoordinateReferenceSystem;
+import org.locationtech.proj4j.CoordinateTransform;
+import org.locationtech.proj4j.CoordinateTransformFactory;
+import org.locationtech.proj4j.ProjCoordinate;
 
 import com.slimjars.dist.gnu.trove.list.array.TLongArrayList;
 
@@ -18,6 +25,7 @@ import de.topobyte.osm4j.core.model.impl.Tag;
 import de.topobyte.osm4j.core.model.impl.Way;
 import de.topobyte.osm4j.xml.output.OsmXmlOutputStream;
 import uk.me.jstott.jcoord.LatLng;
+import uk.me.jstott.jcoord.MGRSRef;
 import uk.me.jstott.jcoord.UTMRef;
 
 public class OsmInteractive {
@@ -63,12 +71,7 @@ public class OsmInteractive {
 
 	public static void readAndWriteNodeDetails(int num) {
 		List<Node> nodeList = new ArrayList<>();
-		double bearing = 0;
-		double originlat = 50.9773653;
-		double originlon = 11.34782554233;
-		double originx = 0;
-		double originy = 0;
-		double mapunitdistance = 0.10;
+		
 		for (int i = 0; i < num; i++) {
 			System.out.println("Enter the node details in the following order : id, x, y");
 			long id = sc.nextLong();
@@ -78,25 +81,6 @@ public class OsmInteractive {
 			double angle = Math.atan2(y, x);
 			double angleAdjusted = angle;
 			// getGeolocation(originlat, originlon, x, y);
-			if (i == 0) {
-				getGeolocations(id, originlat, originlon, 0, 0);
-				originx = y;
-				originy = x;
-			} else {
-				double distance = mapunitdistance
-						* (Math.sqrt(Math.pow((y - originx), 2) + Math.pow((x - originy), 2)));
-				if (y != originx && x == originy) {
-					bearing = 90;
-					getGeolocations(id, originlat, originlon, distance, bearing);
-				} else if (y == originx && x != originy) {
-					bearing = 0;
-					getGeolocations(id, originlat, originlon, distance, bearing);
-				} else if (y != originx && x != originy) {
-					bearing = 45;
-					getGeolocations(id, originlat, originlon, distance, bearing);
-				}
-			}
-
 		}
 
 	}
@@ -126,216 +110,84 @@ public class OsmInteractive {
 		return nodeList;
 	}
 
-	public static void getGeolocations(long i, double lat1, double lon1, double distance, double brng) {
-
-		if (i == -1) {
-			System.out.println(lon1);
-			System.out.println(lat1);
-			osmOutput.write(new Node(i, lon1, lat1));
-
-		} else {
-			lon1 = Math.toRadians(lon1);
-			lat1 = Math.toRadians(lat1);
-			brng = Math.toRadians(brng);
-			double lat2 = Math.asin(Math.sin(lat1) * Math.cos(distance / 6371)
-					+ Math.cos(lat1) * Math.sin(distance / 6371) * Math.cos(brng));
-
-			double lon2 = lon1 + Math.atan2(Math.sin(brng) * Math.sin(distance / 6371) * Math.cos(lat1),
-					Math.cos(distance) - Math.sin(lat1) * Math.sin(lat2));
-
-			double lat2d = Math.toDegrees(lat2);
-			double long2d = Math.toDegrees(lon2);
-
-			osmOutput.write(new Node(i, long2d, lat2d));
-			System.out.println(lat2d);
-			System.out.println(long2d);
-		}
-	}
-	
-	public static void ifcwgs2Utm2wgs (double x, double y, double lat , double lon)
-	{
-		
-		double lat1 = 56.336468;
-		double long1 = 150.311068 ;
-		UTMRef utm =wgstoProjectedCoorsys(lat1, long1);
-		String str = utm.toString();
-		String[] splited = str.split("\\s+");
-		String zone = splited[0];
-		double easting = Double.valueOf(splited[1]);
-		double northing = Double.valueOf(splited[2]);
-		
-		ifclocaltomapcoord(116.611,75.960,easting, northing, zone);
-		
-		
-		
-	}
-
-	public static void ifc2geolocations (double x , double y, double dist) {
-		
-		
-		double lon = 11.34782554233 ;
-		double lat = 50.9773653;
-		double bearing;
-		double distance = dist/1000;
-		// bearing =  Math.toDegrees(Math.atan2(y,x));  
-		 //bearing = (bearing+360)%360;
-		 
-		 
-		
-	
-		
-		if(x==0 && y>0)
-		{
-				bearing = 0;
-		}
-		else if(y==0 && x<0)
-		{
-				bearing = 270;
-		}
-		else if(x==0 && y>0)
-		{
-				bearing = 180;
-		}
-		else if(y==0 && x>0)
-		{
-				bearing = 90;
-		}
-		else
-		{
-		 bearing =   Math.toDegrees(Math.atan2(y,x))    ;                  
-		}
-	/*	else
-		{
-			bearing = 90 - Math.toDegrees(Math.atan2(y,x))    ; 
-		}*/
-		
-		//System.out.println(bearing);
-		
-		 double bearing2 = (bearing+360)%360;
-		 bearing2 = Math.toRadians(bearing2);
-		 System.out.println(bearing2);
-			
-		 
-		double lon1 = Math.toRadians(lon);
-		double lat1 = Math.toRadians(lat);
-		double lat2 = Math.asin(Math.sin(lat1) * Math.cos(distance / 6371)
-				+ Math.cos(lat1) * Math.sin(distance / 6371) * Math.cos(bearing2));
-
-		double lon2 = lon1 + Math.atan2(Math.sin(bearing2) * Math.sin(distance / 6371) * Math.cos(lat1),
-				Math.cos(distance/6371) - Math.sin(lat1) * Math.sin(lat2));
-		
-
-		double lat2d = Math.toDegrees(lat2);
-		double long2d = Math.toDegrees(lon2);
-		
-		
-		//	long2d = (long2d+540)%360;
-			//long2d = long2d -180;
-		
 	
 
-		System.out.println("lat "+lat2d);
-		System.out.println("lon"+long2d);
-		
-		
-	}
-	
-	public static UTMRef wgstoProjectedCoorsys(double lat, double lon) {
-		System.out.println("Convert Latitude/Longitude to UTM Reference");
-		LatLng latlon = new LatLng(lat, lon);
-		// LatLng latlon = new LatLng(53.320555, -1.729000);
-		System.out.println("Latitude/Longitude: " + latlon.toString());
-		UTMRef utm = latlon.toUTMRef();
-		System.out.println("Converted to UTM Ref: " + utm.toString());
-		System.out.println();
-		return utm;
-	}
+	public static ProjCoordinate ifclocalcoord2globalcoordv2(double x, double y, double rotation, double lat,
+			double lon, String epsg) {
 
-	public static void projectedtoWgsCoorsys(double northing, double easting, int zonelon, char zonelat) {
-		System.out.println("Convert UTM Reference to Latitude/Longitude");
-		UTMRef utm = new UTMRef(zonelon, zonelat, easting, northing);
-		// UTMRef utm = new UTMRef(30, 'U', 584662.2085495128, 5908683.746009846);
-		System.out.println("UTM Reference: " + utm.toString());
-		LatLng latlong = utm.toLatLng();
-		System.out.println("Converted to Lat/Long: " + latlong.toString());
-		System.out.println();
-	}
+		long idcount = 0 ;
+		List<CoordinateReferenceSystem> crslist = createCRSfac(epsg);
+		ProjCoordinate projcoord = createCTfac(crslist.get(0), crslist.get(1), lat, lon);
 
-	public static void ifclocaltomapcoord(double lat, double lon, double easting, double northing, String zone) {
+		
+
+		List<Double> mapcoords = ifctomapcoord(x, y, rotation, projcoord.x, projcoord.y);
+		ProjCoordinate projcoordwgs = createCTfac(crslist.get(1), crslist.get(0), mapcoords.get(1), mapcoords.get(0));
+	 idcount = idcount-1;
+		osmOutput.write(new Node(idcount, projcoordwgs.x, projcoordwgs.y));
+		return projcoordwgs;
+
+	}
+	public static ProjCoordinate ifclocalcoordtoglobalcoordv4(double x, double y, double eastings, double northings,
+			double xAxisAbscissa, double xAxisOrdinate, String epsg) {
 		// IFC Map Conversion parameters
+long idcount = 0;
+		double rotation = Math.atan2(xAxisOrdinate, xAxisAbscissa);
 
-		double eastings = easting ;//333780.622;
-		double northings = northing;//6246775.891;
-		double orthogonalHeight = 97.457;
-		double xAxisAbscissa = 0.990330045;
-		double xAxisOrdinate = -0.138731399;
-		double scale = 0.999998;
-		double rotation = Math.atan2(xAxisOrdinate, xAxisAbscissa); // check order
-		String zn = zone;
-		int zonelon = Integer.valueOf(zn.substring(0, 2));
-		char zonelat = zn.charAt(2);
+		double a = Math.cos(rotation);
+		double b = Math.sin(rotation);
 
-		double localx = lat;
-		double localy = lon;
-		double localzh = 0;
+		double eastingsmap = (a * x) - (b * y) + eastings;
+		double northingsmap = (b * x) + (a * y) + northings;
 
-		double a = scale * Math.cos(rotation);
-		double b = scale * Math.sin(rotation);
+		
+		List<CoordinateReferenceSystem> crslist = createCRSfac(epsg);
+		ProjCoordinate projcoordwgs = createCTfac(crslist.get(1), crslist.get(0), northingsmap, eastingsmap);
+		 idcount = idcount-1;
+			osmOutput.write(new Node(idcount, projcoordwgs.x, projcoordwgs.y));
+		return projcoordwgs;
+	}
+
+	private static List<Double> ifctomapcoord(double x, double y, double rotation, double eastings, double northings) {
+		
+		double a = Math.cos(rotation);
+		double b = Math.sin(rotation);
 
 		// map coordinates
-		double eastingsmap = (a * localx) - (b * localy) + eastings;
-		double northingsmap = (b * localx) + (a * localy) + northings;
+		double eastingsmap = (a * x) - (b * y) + eastings;
+		double northingsmap = (b * x) + (a * y) + northings;
 
-		System.out.println("local to Map Grid in x " + eastingsmap);
-		System.out.println("local to Map Grid in y " + northingsmap);
 		
-		projectedtoWgsCoorsys(northingsmap, eastingsmap, zonelon, zonelat);
 
-		double height = localzh + orthogonalHeight;
+		List<Double> mapcoords = new ArrayList<>();
+
+		mapcoords.add(eastingsmap);
+		mapcoords.add(northingsmap);
+
+		return mapcoords;
 	}
 
-	public static void ifc2global2Dhelmert(double x, double y, double z) {
-		// 4 parameters 2D Helmerts transformation
-		// Assumption of N, E known for the origin of the Engineering coordinate system
-		double originEastings = 333780.622;
-		double originNorthing = 6246775.891;
-		double ref2Eastings = 333906.644;
-		double ref2Northing = 6246834.938;
-		double xtranslation = originEastings;
-		double ytranslation = originNorthing;
-		double scale = 0.999998;
+	private static List<CoordinateReferenceSystem> createCRSfac(String epsg) {
 
-		// local coordinates
-		double ref1x = 0;
-		double ref1y = 0;
-		double ref2x = 116.611;
-		double ref2y = 75.960;
+		CRSFactory crsFactory = new CRSFactory();
+		CoordinateReferenceSystem wgs84 = crsFactory.createFromName("epsg:4326");
+		CoordinateReferenceSystem ifcproj = crsFactory.createFromName(epsg);
 
-		// using 2D helmerts transformation, solving simultaneous equations
+		List<CoordinateReferenceSystem> crs = Arrays.asList(wgs84, ifcproj);
+		return crs;
+	}
 
-		double val1 = Math.pow(ref2x, 2) + Math.pow(ref2y, 2);
-		double val2 = (ref2Eastings - originEastings) * ref2x + (ref2Northing - originNorthing) * ref2y;
+	private static ProjCoordinate createCTfac(CoordinateReferenceSystem crs1, CoordinateReferenceSystem crs2,
+			double xcoord, double ycoord) {
 
-		double val3 = val2 / val1;
-
-		double localx = x;
-		double localy = y;
-		double localzh = z;
-		// solving for theta , assuming scale =1
-
-		double angle = Math.acos(val3);
-
-		double a = scale * Math.cos(angle);
-		double b = scale * Math.sin(angle);
-
-		double eastingsmap = (a * localx) - (b * localy) + xtranslation;
-		double northingsmap = (b * localx) + (a * localy) + ytranslation;
-
-		System.out.println("local to Map Grid Helmert in x " + eastingsmap);
-		System.out.println("local to Map Grid Helmert in y " + northingsmap);
-
-		// double height = localzh + orthogonalHeight;
+		CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+		CoordinateTransform coordtransformer = ctFactory.createTransform(crs1, crs2);
+		ProjCoordinate resultcoord = new ProjCoordinate();
+		coordtransformer.transform(new ProjCoordinate(ycoord, xcoord), resultcoord);
+		return resultcoord;
 
 	}
+
+	
 
 }
--- src/test/java/org/opensourcebim/levelout/UtilityTests.java
+++ src/test/java/org/opensourcebim/levelout/UtilityTests.java
@@ -3,6 +3,7 @@ package org.opensourcebim.levelout;
 import de.topobyte.osm4j.xml.output.OsmXmlOutputStream;
 import org.junit.Assert;
 import org.junit.Test;
+import org.locationtech.proj4j.ProjCoordinate;
 import org.opensourcebim.levelout.samples.OsmInteractive;
 import org.opensourcebim.levelout.util.CoordinateConversion;
 import org.opensourcebim.levelout.util.CoordinateConversion.CartesianPoint;
@@ -11,8 +12,8 @@ import org.opensourcebim.levelout.util.CoordinateConversion.GeodeticPoint;
 import java.util.Scanner;
 
 public class UtilityTests {
-	@Test
-	public void testGetGeoLocations(){
+/*	@Test
+	public void testGetGeoLocations() {
 		OsmInteractive.osmOutput = new OsmXmlOutputStream(System.out, true);
 		OsmInteractive.getGeolocations(-1, 50.9773653, 11.34782554233, 0, 0);
 		OsmInteractive.getGeolocations(-2, 50.9773653, 11.34782554233, 6, 90);
@@ -20,8 +21,9 @@ public class UtilityTests {
 		OsmInteractive.getGeolocations(-4, 50.9773653, 11.34782554233, 6, 45);
 		OsmInteractive.osmOutput.complete();
 	}
+
 	@Test
-	public void testInteractive(){
+	public void testInteractive() {
 		OsmInteractive.osmOutput = new OsmXmlOutputStream(System.out, true);
 		OsmInteractive.sc = new Scanner("-1 0 0\n-2 0 6\n-3 6 6\n-4 6 0\n");
 		OsmInteractive.readAndWriteNodeDetails(4);
@@ -29,34 +31,33 @@ public class UtilityTests {
 	}
 
 	@Test
-	public void testJCoord(){
+	public void testJCoord() {
 		OsmInteractive.wgstoProjectedCoorsys(53.320555, -1.729000);
 		OsmInteractive.projectedtoWgsCoorsys(5908683.746009846, 584662.2085495128, 30, 'U');
-		//OsmInteractive.ifclocaltomapcoord(6,6);
+		// OsmInteractive.ifclocaltomapcoord(6,6);
 	}
 
 	@Test
-	public void testCartesianToWgs84Origin(){
-		CartesianPoint pointToTransform = new CartesianPoint(0,0,0);
+	public void testCartesianToWgs84Origin() {
+		CartesianPoint pointToTransform = new CartesianPoint(0, 0, 0);
 		GeodeticPoint csOrigin = new GeodeticPoint(53.320555, -1.729000, 0);
 		GeodeticPoint transformed = CoordinateConversion.convertCartesianToWGS84(csOrigin, pointToTransform);
 		Assert.assertEquals(csOrigin.longitude, transformed.longitude, 0.000000001);
 		Assert.assertEquals(csOrigin.latitude, transformed.latitude, 0.000000001);
 		Assert.assertEquals(csOrigin.height, transformed.height, 0.000000001);
-	} 
-	
-	public void testifc2global2Dhelmert()
-	{
-		OsmInteractive.ifc2global2Dhelmert(116.611, 75.960, 0);
-		//OsmInteractive.ifclocaltomapcoord(116.611, 75.960);
-	} 
-	
-	public void testifc2geolocations() 
-	{
-		//OsmInteractive.ifc2geolocations(0, 6, 60);
-	//	OsmInteractive.ifc2geolocations(6, 0, 60);
-		//OsmInteractive.ifc2geolocations(6, 6, 60);
-		//OsmInteractive.ifc2geolocations(0, 6, 60);
-		OsmInteractive.ifcwgs2Utm2wgs(0, 0, 0, 0);
+	}*/
+
+	@Test
+	public void testifc2geolocations() {
+
+		ProjCoordinate wgsV2 = OsmInteractive.ifclocalcoord2globalcoordv2(116.611, 75.960, -0.13918031137,
+				-33.90632062825244, 151.20215639320887, "epsg:28356");
+		ProjCoordinate wgsV4 = OsmInteractive.ifclocalcoordtoglobalcoordv4(116.611, 75.960, 333780.62200000236,
+				6246775.890999999, 0.990330045, -0.138731399, "epsg:28356");
+		Assert.assertEquals(wgsV2.x, 151.203530034203, 0.000000001);
+		Assert.assertEquals(wgsV2.y, -33.9058082188758, 0.000000001);
+		Assert.assertEquals(wgsV4.x, 151.203530034203, 0.000000001);
+		Assert.assertEquals(wgsV4.y, -33.9058082188758, 0.000000001);
+
 	}
 }
