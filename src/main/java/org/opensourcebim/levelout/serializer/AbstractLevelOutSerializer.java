54572608e1de57ec9e401ae1a5dd070831768cc0
hlg <public.void@gmx.de>
2023-01-28 12:01:07 +0100

    initialize CRS in serializer

--- src/main/java/org/opensourcebim/levelout/serializer/AbstractLevelOutSerializer.java
+++ src/main/java/org/opensourcebim/levelout/serializer/AbstractLevelOutSerializer.java
@@ -6,10 +6,14 @@ import org.bimserver.models.ifc4.*;
 import org.bimserver.plugins.serializers.ProjectInfo;
 import org.bimserver.plugins.serializers.Serializer;
 import org.bimserver.plugins.serializers.SerializerException;
+import org.eclipse.emf.common.util.EList;
 import org.opensourcebim.levelout.intermediatemodel.Storey;
 import org.opensourcebim.levelout.intermediatemodel.Building;
 import org.opensourcebim.levelout.intermediatemodel.Corner;
 import org.opensourcebim.levelout.intermediatemodel.Room;
+import org.opensourcebim.levelout.util.CoordinateConversion.CoordinateReference;
+import org.opensourcebim.levelout.util.CoordinateConversion.GeodeticOriginCRS;
+import org.opensourcebim.levelout.util.CoordinateConversion.GeodeticPoint;
 
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -20,13 +24,13 @@ import java.util.List;
 public abstract class AbstractLevelOutSerializer implements Serializer {
     boolean extractionMethod = false;
     Building building;
-
+    CoordinateReference crs;
     AbstractLevelOutSerializer(boolean extractionMethod){
         this.extractionMethod = extractionMethod;
     }
     @Override
     public void init(IfcModelInterface ifcModelInterface, ProjectInfo projectInfo, boolean b) throws SerializerException {
-        if(extractionMethod) initStructure(ifcModelInterface); else initSample(); // TODO: depending on serializer settings use other init function
+        if(extractionMethod) initStructure(ifcModelInterface); else initSample();
     }
     private void initSample(){
         Corner p1 = new Corner(0,0,0.,0);
@@ -51,6 +55,7 @@ public abstract class AbstractLevelOutSerializer implements Serializer {
                 new Room(5, "floor", Arrays.asList( p14,p13,p15,p16 ))
             ), Collections.emptyList())
         ));
+        crs = new GeodeticOriginCRS(new GeodeticPoint(53.320555, -1.729000, 0), -0.13918031137);
     }
     private void initStructure(IfcModelInterface ifcModelInterface){
         List<Storey> loStoreys = new ArrayList<>();
@@ -71,7 +76,34 @@ public abstract class AbstractLevelOutSerializer implements Serializer {
                 }
             }
         }
+        GeodeticOriginCRS geoCRS = getGeodeticCRS(ifcModelInterface);  // TODO check for map conversion (first, before geo)
+        crs = geoCRS != null ? geoCRS : new GeodeticOriginCRS(new GeodeticPoint(0,0,0), 0); // TODO use some default location in Weimar, Dresden ..
     }
+
+    private GeodeticOriginCRS getGeodeticCRS(IfcModelInterface ifcModelInterface) {
+        // TODO common classes for checking and querying
+        List<IfcSite> site = ifcModelInterface.getAllWithSubTypes(IfcSite.class);
+        List<IfcProject> project = ifcModelInterface.getAllWithSubTypes(IfcProject.class);
+        if( !(project.size() == 1)) return null;
+        if( !(site.size() == 1)) return null;
+        if( !(project.get(0).getIsDecomposedBy().size() == 1)) return null;
+        if( !project.get(0).getIsDecomposedBy().get(0).getRelatingObject().getGlobalId().equals(site.get(0).getGlobalId())) return null; // or even identity on entity level?
+        IfcDirection trueNorth = ((IfcGeometricRepresentationContext) project.get(0).getRepresentationContexts().get(0)).getTrueNorth();
+        if( !(trueNorth.getDim() == 2 && trueNorth.getDirectionRatios().size() == 2)) return null;
+        EList<Long> refLatitude = site.get(0).getRefLatitude();
+        if( !(refLatitude !=null && refLatitude.size()>=3 )) return null;
+        EList<Long> refLongitude= site.get(0).getRefLongitude();
+        if (! (refLongitude !=null && refLongitude.size()>=3)) return null;
+        double rotation = Math.atan2(trueNorth.getDirectionRatios().get(1), trueNorth.getDirectionRatios().get(0));
+        double latitude = degreesFromMinutes(refLatitude);
+        double longitude = degreesFromMinutes(refLongitude);
+        return new GeodeticOriginCRS(new GeodeticPoint(latitude, longitude, 0), rotation);
+    }
+
+    private static double degreesFromMinutes(EList<Long> refLatitude) {
+        return refLatitude.get(0) + refLatitude.get(1) / 60. + refLatitude.get(2) / 3600.; // TODO consider optional microseconds
+    }
+
     @Override
     public boolean write(OutputStream outputStream) throws SerializerException, BimserverDatabaseException {
         this.writeToOutputStream(outputStream, null);
--- src/main/java/org/opensourcebim/levelout/serializer/OsmSerializer.java
+++ src/main/java/org/opensourcebim/levelout/serializer/OsmSerializer.java
@@ -3,9 +3,6 @@ package org.opensourcebim.levelout.serializer;
 import org.bimserver.plugins.serializers.ProgressReporter;
 import org.bimserver.plugins.serializers.SerializerException;
 import org.opensourcebim.levelout.builders.OsmBuilder;
-import org.opensourcebim.levelout.util.CoordinateConversion.CoordinateReference;
-import org.opensourcebim.levelout.util.CoordinateConversion.GeodeticOriginCRS;
-import org.opensourcebim.levelout.util.CoordinateConversion.GeodeticPoint;
 
 import java.io.OutputStream;
 
@@ -17,8 +14,6 @@ public class OsmSerializer extends AbstractLevelOutSerializer {
     @Override
     public void writeToOutputStream(OutputStream outputStream, ProgressReporter progressReporter) throws SerializerException {
         try {
-            CoordinateReference crs = new GeodeticOriginCRS(new GeodeticPoint(53.320555, -1.729000, 0), -0.13918031137);
-            // TODO populate earlier and correctly for sample and IFC input
             new OsmBuilder().createAndWriteBuilding(building, crs, outputStream);
         } catch (Exception e) {
             throw new SerializerException(e);
