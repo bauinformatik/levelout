cae7ba65033d510010871d170ae5a59145766cce
soxe8057 <soxe8057@BILLION.bauing.uni-weimar.de>
2023-03-07 12:18:45 +0100

    Geodata analysis 2 + IfcSpace analysis

--- src/main/java/org/opensourcebim/levelout/checkingservice/LevelOutChecking.java
+++ src/main/java/org/opensourcebim/levelout/checkingservice/LevelOutChecking.java
@@ -1,22 +1,31 @@
 package org.opensourcebim.levelout.checkingservice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.bimserver.emf.IdEObject;
 import org.bimserver.emf.IfcModelInterface;
 import org.bimserver.interfaces.objects.SObjectType;
 import org.bimserver.interfaces.objects.SProject;
 import org.bimserver.interfaces.objects.SVector3f;
+import org.bimserver.models.ifc4.IfcGeometricRepresentationContext;
 import org.bimserver.models.ifc4.IfcMapConversion;
 import org.bimserver.models.ifc4.IfcProjectedCRS;
+import org.bimserver.models.ifc4.IfcSpace;
 import org.bimserver.plugins.SchemaName;
 import org.bimserver.plugins.services.AbstractAddExtendedDataService;
 import org.bimserver.plugins.services.BimServerClientInterface;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com.google.common.collect.BiMap;
 
 
 public class LevelOutChecking extends AbstractAddExtendedDataService {
@@ -29,10 +38,10 @@ public class LevelOutChecking extends AbstractAddExtendedDataService {
         SProject project = bimServerClientInterface.getServiceInterface().getProjectByPoid(poid);
         IfcModelInterface model = bimServerClientInterface.getModel(project, roid, true, false, false);
         
-        long entities = model.size();
+        long entities_No = model.size();
         //String ifcEntities = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcRoot")).toString();
         
-        long products = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcProduct")).size();
+        long products_No = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcProduct")).size();
         //String ifcProducts = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcProduct")).toString();
         
         
@@ -44,166 +53,155 @@ public class LevelOutChecking extends AbstractAddExtendedDataService {
         txt.append("Hallo Welt! Ich bin das Prüfroutinen-Plugin :)\n");
         txt.append("-------------------\n");
         txt.append("Current project: ").append(project.getName()).append("\n");
-        txt.append("No of entities: ").append(entities).append("\n");
-        //txt.append("Names of entities: ").append(ifcEntities).append("\n");
-        txt.append("No of products: ").append(products).append("\n");
-        //txt.append("Names of products: ").append(ifcProducts).append("\n");
-        
+        txt.append("No of entities: ").append(entities_No).append("\n");
+        txt.append("No of products: ").append(products_No).append("\n");
         txt.append("\n");
         
+        txt.append("Bounds values: \n");
         if(mnBnds!= null && mxBnds != null) txt.append("Bounds: " +
                 mnBnds.getX() + ", " + mnBnds.getY() + ", " + mnBnds.getZ() + " --- " +
                 mxBnds.getX() + ", " + mxBnds.getY() + ", " + mxBnds.getZ() + "\n");
         else txt.append("Bounds: no bounds set\n");
-        
         txt.append("\n");
         
-        //Überprüfung auf das Vorhandensein von IfcMapConversion
-        //String entityInstances = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcProduct")).toString();
         
-        //List<Object> entityInstances = (List<Object>) EcoreUtil.getObjectsByType(model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcProduct")), model.getPackageMetaData().getEClass("IfcProduct"));
-        //txt.append("Folgende Entitäten sind enthalten: ").append(entityInstances).append("\n");
-        
-        //Collection<IdEObject> entityInstances = model.getObjects();
-        //Collection<IdEObject> entityInstances = model.getObjects();
-        
-        List<IdEObject> entityInstances = model.getAllWithSubTypes(model.getPackageMetaData().getEClass("IfcCoordinateOperation"));
-        //txt.append("List of entity instances: ").append(entityInstances).append("\n");
-       
-        boolean hasIfcMapConversion = false;
-
-        for (IdEObject entityInstance : entityInstances) {
-        	if (entityInstance instanceof IfcMapConversion) {
-            	hasIfcMapConversion = true;
-                break;
-            }
-        }
-
-        txt.append("hasIfcMapConversion is ").append(hasIfcMapConversion).append("\n");;
-        
-        if (hasIfcMapConversion) {
-            txt.append("IFC model contains IfcMapConversion.").append("\n");
-        } else {
-            txt.append("IFC model does not contain IfcMapConversion.").append("\n");
-        }
-        
+        //Ausgabe aller Entitäten, die im IFC-Modell enthalten sind
+        BiMap<Long, IdEObject> entities = model.getObjects();
+        //txt.append("Folgende Entitäten sind enthalten: ").append(entities).append("\n");
+        long entities_size = entities.size();
+        txt.append("Die Anzahl der Entitäten über die Methode getObjects() beträgt: ").append(entities_size).append("\n");
         txt.append("\n");
         
-        //Auslesen von IfcMapConversion und IfcProjectedCRS für die Validierung und Ausgabe der Geodaten
-        txt.append("Überprüfung von IfcMapConversion").append("\n");
+        Collection<IdEObject> entities2 = model.getValues();
+        //txt.append("Folgende Entitäten sind enthalten: ").append(entitiess).append("\n");
+        long entitiess_size = entities2.size();
+        txt.append("Die Anzahl der Entitäten über die Methode getValues() beträgt: ").append(entitiess_size).append("\n");
+        txt.append("\n\n");
+ 
         
-        List<IfcMapConversion> mapConversions = model.getAll(IfcMapConversion.class);
-        
-        List<String> featureNames = List.of("SourceCRS", "TargetCRS", "Eastings", "Northings", "OrthogonalHeight", "XAxisAbscissa", "XAxisOrdinate", "Scale");
-            
-        for (IfcMapConversion mapConversion : mapConversions) {
-       
-        	if (featureNames.stream().anyMatch(featureName -> 
-        		mapConversion.eGet(mapConversion.eClass().getEStructuralFeature(featureName)) == null)) {
-				txt.append("Die Geodaten im IFC-Modell sind ungültig. Folgende Geodaten fehlen und müssen ergänzt werden: ").append("\n");
-				featureNames.stream()
-                .filter(featureName -> mapConversion.eGet(mapConversion.eClass().getEStructuralFeature(featureName)) == null)
-                .forEach(featureName -> txt.append(featureName + " fehlt\n"));
-
-            } else
-				txt.append("Die Geodaten im IFC-Modell sind gültig").append("\n");
-            txt.append("\n");
-        	
-            	
-            
-        	
-        	/*txt.append("SourceCRS: " + mapConversion.getSourceCRS()).append("\n");
-            txt.append("TargetCRS: " + mapConversion.getTargetCRS()).append("\n");
-            txt.append("Easting: " + mapConversion.getEastings()).append("\n"); //Spielt scheinbar keine Rolle, ob man es als String oder double ausgeben lässt.
-            txt.append("Northing: " + mapConversion.getNorthings()).append("\n"); //Spielt scheinbar keine Rolle, ob man es als String oder double ausgeben lässt.
-            txt.append("OrthogonalHeight: " + mapConversion.getOrthogonalHeight()).append("\n");
-            txt.append("XAxisAbscissa: " + mapConversion.getXAxisAbscissa()).append("\n");
-            txt.append("XAxisOrdinate: " + mapConversion.getXAxisOrdinate()).append("\n");
-            txt.append("Scale: " + mapConversion.getScale()).append("\n");
-            txt.append("\n");
-            
-            Map<Supplier<Object>, String> fields = new LinkedHashMap<>();
-            fields.put(mapConversion::getSourceCRS, "SourceCRS");
-            fields.put(mapConversion::getTargetCRS, "TargetCRS");
-            fields.put(mapConversion::getEastings, "Eastings");
-            fields.put(mapConversion::getNorthings, "Northings");
-            fields.put(mapConversion::getOrthogonalHeight, "OrthogonalHeight");
-            fields.put(mapConversion::getXAxisAbscissa, "XAxisAbscissa");
-            fields.put(mapConversion::getXAxisOrdinate, "XAxisOrdinate");
-        	
-            if (fields.entrySet().stream().anyMatch(entry -> entry.getKey().get() == null)) {
-				txt.append("Die Geodaten im IFC-Modell sind ungültig. Folgende Geodaten fehlen und müssen ergänzt werden: ").append("\n");
-				fields.entrySet().stream()
-                .filter(entry -> entry.getKey().get() == null)
-                .forEach(entry -> txt.append(entry.getValue() + " fehlt\n"));
-
-            } else
-				txt.append("Die Geodaten im IFC-Modell sind gültig").append("\n");*/
-            txt.append("\n");
+        //Auslesen von IfcMapConversion und IfcProjectedCRS für die Ausgabe und Validierung der Geodaten
+        txt.append("Ausgabe und Validierung der Geodaten\n\n");
+        //Überprüfung auf das Vorhandensein von IfcMapConversion und IfcProjectedCRS sowie deren Attributwerte
+        //Wie kann die folgende Zeile noch generisch umgestaltet werden, damit die Werte, die aus der DB abgefragtwerden sollen auch in diesem String erscheinen.
+        //Problem sind die Werte im IFC-Modell nicht vorhanden, werden ise auch nicht in der Liste gespeichert und demnach auch nicht in einem String, der die Liste abfragt.
+        txt.append("Überprüfung auf das Vorhandensein von IfcMapConversion, IfcProjectedCRS und IfcGeometricRepresentationContext sowie deren Attributwerte\n");
+        List<IdEObject> entities3 = new ArrayList<IdEObject>(
+        	Arrays.asList(
+        		model.getAll(IfcMapConversion.class).stream().findFirst().orElse(null),//IfcMapConversion
+        		model.getAll(IfcMapConversion.class).stream().findFirst().orElse(null).getSourceCRS(),//IfcGeometricRepresentationContext
+        		model.getAll(IfcMapConversion.class).stream().findFirst().orElse(null).getTargetCRS()//IfcProjectedCRS
+        		//Methode finden zum Abfragen der weiteren Attribute
+        		//model.getAll(IfcMapConversion.class).stream().findFirst().orElse(null).getSourceCRS(),
+        	)
+        );
+        for (IdEObject entity : entities3) {
+	        if (entity != null) {
+	        	EClass eClass = entity.eClass();
+	        	EList<EStructuralFeature> eFeatures = eClass.getEAllStructuralFeatures();
+	        	txt.append("\n").append("Die Attribute von " + entity.eClass().getName().toString() + " lauten: \n");
+	        	for (EStructuralFeature eFeature : eFeatures) {
+	        		 String featureName = eFeature.getName();
+	        		 Object featureValue = entity.eGet(eFeature);
+	        		 txt.append("\t").append(featureName).append(": ").append(featureValue).append("\n");
+	        	}
+	        	txt.append("\n");
+	        	
+	        	//Beginn der Prüfbedingungen
+	        	//Abspeichern der zu überprüfenden Attribute in Liste validValues mit für die Geolokalisation notwendigen Attribute
+	        	List<String> validValues = new ArrayList<>(
+	        			Arrays.asList(
+	        					"SourceCRS", "TargetCRS", "Eastings","Northings", "OrthogonalHeight", "XAxisAbscissa", "XAxisOrdinate", //IfcMapConversion
+	        					"WorldCoordinateSystem", //IfcGeometricRepresentationContext
+	        					"GeodeticDatum", "VerticalDatum", "MapProjection", "MapZone", "MapUnit" //IfcProjectedCRS
+    					)
+	        	);
+	        	//Überprüfung1 auf Attributwerte in Liste validValues != null
+	        	boolean hasValue = eFeatures.stream()
+	        			//Filtern nach den Werten in der Liste validValues
+	        			.filter(feature -> validValues.contains(feature.getName()))
+	        			.map(feature -> entity.eGet(feature))
+	        			//Überprüfung, ob die Werte nicht null sind
+	        			.allMatch(value -> value != null);//oder anyMatch()?
+	        			//.filter --> Alle rausschreiben, die Fehlerhaft sind, wenn Liste leer alle gültig, wenn etwas drin ist, Fehler vorhanden
+	        	//Überprüfung2 auf Gültigkeit der Geodaten, also IFC-Modell in Bezug auf Geodaten gültig oder ungültig
+	        	if (hasValue) {
+	        	    txt.append("Die erforderlichen Geodaten im IFC-Modell sind gültig.\n");
+	        	} else {
+	        		//Überprüfung3 auf Notwendigkeit der Ergänzung
+	        		txt.append("Die erforderlichen Geodaten im IFC-Modell sind ungültig.\n");
+	        		txt.append("Folgende Geodaten müssen ergänzt werden: \n");
+	        	    eFeatures.stream()
+	        	    .filter(feature -> entity.eGet(feature) == null)
+	        	    .forEach(feature -> txt.append("\t").append(feature.getName()).append(" fehlt\n"));
+	        	}
+	        	
+	        	
+	        	/*boolean hasNullValue = eFeatures.stream()
+	        			//Filtern für die Ausnahme für Überprüfung3
+	        			.filter(feature -> !(feature.getName() == "ScaleAsString") || "ContextIdentifier") // Ausnahme für ScaleAsString
+	        			.map(feature -> entity.eGet(feature)).anyMatch(value -> value == null);
+	        	if (hasNullValue) {
+	        		//Überprüfung2 auf Gültigkeit der Geodaten, also IFC-Modell in Bezug auf Geodaten gültig oder ungültig
+	        	    txt.append("Die Geodaten im IFC-Modell sind ungültig.\n");
+	        		txt.append("Folgende Geodaten müssen ergänzt werden: \n");
+	        	    eFeatures.stream()
+	        	    //Überprüfung3 auf Notwendigkeit der Ergänzung
+	        	    	.filter(feature -> !feature.getName().equals("ScaleAsString") && entity.eGet(feature) == null)
+	        	        .forEach(feature -> txt.append("\t").append(feature.getName()).append(" fehlt\n"));
+	        	} else {
+	        		txt.append("Die erforderlichen Geodaten im IFC-Modell sind gültig").append("\n");
+	        	}*/
+	        	
+	        } else {
+	        	txt.append("Die Entität IfcMapConversion, IfcProjectedCRS oder IfcGeometricRepresentationContext fehlt im IFC-Modell.\n");
+	        }
         }
-        
-        txt.append("Überprüfung von IfcProjectedCRS").append("\n");
-        
-        List<IfcProjectedCRS> projectedCRSs = model.getAll(IfcProjectedCRS.class);
-        
-        for (IfcProjectedCRS projectedCRS : projectedCRSs) {
-            txt.append("Name: " + projectedCRS.getName()).append("\n");
-        	txt.append("Description: " + projectedCRS.getDescription()).append("\n");
-        	txt.append("GeodeticDatum: " + projectedCRS.getGeodeticDatum()).append("\n");
-        	txt.append("VerticalDatum: " + projectedCRS.getVerticalDatum()).append("\n");
-        	txt.append("MapProjection: " + projectedCRS.getMapProjection()).append("\n");
-        	txt.append("MapZone: " + projectedCRS.getMapZone()).append("\n");
-        	txt.append("MapUnit: " + projectedCRS.getMapUnit()).append("\n");
-        	txt.append("\n");
-            
-        	txt.append("List all attribute names of IfcProjectedCRS: " + model.getAll(IfcProjectedCRS.class)).append("\n");
-        	txt.append("\n");
-        	
-            /*if (projectedCRS.getName() == null) {
-          	    txt.append("Name fehlt").append("\n");
-            }
-            if (projectedCRS.getDescription() == null) {
-          	    txt.append("Description fehlt").append("\n");
-            }
-            if (projectedCRS.getGeodeticDatum() == null) {
-          	    txt.append("GeodeticDatum fehlt").append("\n");
-            }
-            if (projectedCRS.getVerticalDatum() == null) {
-            	txt.append("VerticalDatum fehlt").append("\n");
-            }
-            if (projectedCRS.getMapProjection() == null) {
-            	txt.append("MapProjection fehlt").append("\n");
-            }
-            if (projectedCRS.getMapZone() == null) {
-                txt.append("MapZone fehlt").append("\n");
-            }
-            if (projectedCRS.getMapUnit() == null) {
-              	txt.append("MapUnit fehlt").append("\n");
-            }
-            txt.append("\n");*/
-            
-            Map<Supplier<Object>, String> fields = new LinkedHashMap<>();
-            fields.put(projectedCRS::getName, "Name");
-            fields.put(projectedCRS::getDescription, "Description");
-            fields.put(projectedCRS::getGeodeticDatum, "GeodeticDatum");
-            fields.put(projectedCRS::getVerticalDatum, "VerticalDatum");
-            fields.put(projectedCRS::getMapProjection, "MapProjection");
-            fields.put(projectedCRS::getMapZone, "MapZone");
-            fields.put(projectedCRS::getMapUnit, "MapUnit");
+	        txt.append("\n\n");
 
-            if (fields.entrySet().stream().anyMatch(entry -> entry.getKey().get() == null)) {
-				txt.append("Die Geodaten im IFC-Modell sind ungültig. Folgende Geodaten fehlen und müssen ergänzt werden: ").append("\n");
-				fields.entrySet().stream()
-                .filter(entry -> entry.getKey().get() == null)
-                .forEach(entry -> txt.append(entry.getValue() + " fehlt\n"));
+	        
+	    //Auslesen von IfcSpace für die Ausgabe und Validierung des geometrischen Konzepts
+	    txt.append("Ausgabe und Validierung des geometrischen Konzepts\n\n");
+	    List<IdEObject> entities4 = new ArrayList<>(
+	    	    Stream.concat(
+	    	        model.getAll(IfcSpace.class).stream().findFirst().stream(),
+	    	        model.getAll(IfcSpace.class).stream().findFirst().stream()
+	    	            .flatMap(space -> space.getRepresentation().getRepresentations().stream())
+	    	            .flatMap(rep -> rep.getItems().stream())
+	    	    ).collect(Collectors.toList())
+	    	);
+	    Optional<IdEObject> entitycheck2 = entities4.stream().filter(entity -> entity instanceof IfcSpace).findFirst();
+	    if (entitycheck2.isPresent()) {
+	    	for (IdEObject entity : entities4) {
+	    		EClass eClass = entity.eClass();
+	        	EList<EStructuralFeature> eFeatures = eClass.getEAllStructuralFeatures();
+	        	//Überprüfung auf das Vorhandensein von IfcSpace und dem zugrundeliegenden geometrischen Konzept
+	        	txt.append("Überprüfung auf das Vorhandensein von " + entity.eClass().getName().toString() + " und dem zugrundeliegenden geometrischen Konzept\n");
+	        	txt.append("\n").append("Die Attribute von " + entity.eClass().getName().toString() + " lauten: \n");
+	        	for (EStructuralFeature eFeature : eFeatures) {
+	        		 String featureName = eFeature.getName();
+	        		 Object featureValue = entity.eGet(eFeature);
+	        		 txt.append("\t").append(featureName).append(": ").append(featureValue).append("\n");
+	        	}
+	        	txt.append("\n");
+	        //Beginn der Prüfbedingungen
+	        	//Überprüfung1 auf null-Attributwerte
+	        	boolean hasNullValue = eFeatures.stream().map(feature -> entity.eGet(feature)).anyMatch(value -> value == null);
+	        	if (hasNullValue) {
+	        		txt.append("Die Daten von " + entity.eClass().getName().toString() + " im IFC-Modell sind ungültig. Folgende Daten von " + entity.eClass().getName().toString() + " fehlen und müssen ergänzt werden: \n");
+	        	    eFeatures.stream()
+	        	    	.filter(feature -> entity.eGet(feature) == null)
+	        	        .forEach(feature -> txt.append("\t").append(feature.getName()).append(" fehlt\n"));
+	        	} else {
+	        		txt.append("Die Daten von " + entity.eClass().getName().toString() + " im IFC-Modell sind gültig").append("\n");
+	        	}
+	    	}
+	    } else {
+	    	txt.append("Die Entität " + entitycheck2.toString() + " fehlt im IFC-Modell.\n");
+	    }
+	        txt.append("\n");
 
-            } else
-				txt.append("Die Geodaten im IFC-Modell sind gültig").append("\n");
-            
-            txt.append("\n");
-        
-        }
-        
+	        
+	        
         addExtendedData(txt.toString().getBytes(), "stats.txt", "Statistics", "text/plain", bimServerClientInterface, roid);
         
     }
