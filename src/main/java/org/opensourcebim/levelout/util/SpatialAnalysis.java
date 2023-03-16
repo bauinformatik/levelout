edf2343ad3d0785ca3e09dfd3bc2280cd3f2594f
hlg <public.void@gmx.de>
2023-03-16 22:33:21 +0100

    spatial analysis refactor, bring back tests, make it work for slanted walls

--- src/main/java/org/opensourcebim/levelout/util/SpatialAnalysis.java
+++ src/main/java/org/opensourcebim/levelout/util/SpatialAnalysis.java
@@ -1,8 +1,5 @@
 package org.opensourcebim.levelout.util;
 
-import net.opengis.indoorgml.core.v_1_0.CellSpaceBoundaryPropertyType;
-import net.opengis.indoorgml.core.v_1_0.CellSpaceType;
-import net.opengis.indoorgml.core.v_1_0.StatePropertyType;
 import org.opensourcebim.levelout.intermediatemodel.Corner;
 import org.opensourcebim.levelout.intermediatemodel.Door;
 import org.opensourcebim.levelout.intermediatemodel.Room;
@@ -10,10 +7,6 @@ import org.opensourcebim.levelout.intermediatemodel.Room;
 import java.util.*;
 
 public class SpatialAnalysis {
-	static boolean isDoorOnSegment(Door door, Corner corner1, Corner corner2) { // Corner p1, p2, p3
-		return isColinear(door.getCorners().get(0), corner1, corner2)
-				&& isColinear(door.getCorners().get(1), corner1, corner2);
-	}
 
 	public static Map<Door, List<Room>> analyzeRooms(List<Room> rooms, List<Door> doors) {
 
@@ -22,79 +15,45 @@ public class SpatialAnalysis {
 		for (Door door : doors) {
 			for (Room room : rooms) {
 				if (isDoorInRoom(room, door)) {
-
 					if (!assignment.containsKey(door)) {
 						assignment.put(door, new ArrayList<>());
 					}
-
 					assignment.get(door).add(room);
 				}
 			}
 		}
-
 		return assignment;
 	}
 
 	public static boolean isDoorInRoom(Room room, Door door) {
-
-		Corner doorcorner1 = door.getCorners().get(0);
-		Corner doorcorner2 = door.getCorners().get(1);
-		double slopedoor = (doorcorner2.getY() - doorcorner1.getY()) / (doorcorner2.getX() - doorcorner1.getX());
 		for (int i = 0; i < room.getCorners().size(); i++) {
-			// look at last point / pair as well
-			// Check cross product formula
-
 			int k = (i + 1) % (room.getCorners().size());
-			Corner roomcorner1 = room.getCorners().get(i);
-			Corner roomcorner2 = room.getCorners().get(k);
-			double sloperoom = (roomcorner2.getY() - roomcorner1.getY()) / (roomcorner2.getX() - roomcorner1.getX());
-			double crosspro = ((doorcorner1.getY() - roomcorner1.getY()) * (roomcorner2.getX() - roomcorner1.getX()))
-					+ ((doorcorner1.getX() - roomcorner1.getX()) * (roomcorner2.getY() - roomcorner1.getY()));
-			if (sloperoom == slopedoor || sloperoom == -slopedoor) // check how to avoid minus values , case infinity
-																	// and -infinity// the lines are parallel or
-																	// coincident
-			{
-				if ((roomcorner1.getX() == doorcorner1.getX() && roomcorner1.getX() == doorcorner2.getX())
-						|| (roomcorner1.getY() == doorcorner1.getY() && roomcorner1.getY() == doorcorner2.getY())
-						|| crosspro == 0 || crosspro == -0) {
-
-					double dotpro1 = ((doorcorner1.getX() - roomcorner1.getX())
-							* (roomcorner2.getX() - roomcorner1.getX()))
-							+ ((doorcorner1.getY() - roomcorner1.getY()) * (roomcorner2.getY() - roomcorner1.getY()));
-
-					double dotpro2 = ((doorcorner2.getX() - roomcorner1.getX())
-							* (roomcorner2.getX() - roomcorner1.getX()))
-							+ ((doorcorner2.getY() - roomcorner1.getY()) * (roomcorner2.getY() - roomcorner1.getY()));
-
-					if (dotpro1 >= 0 && dotpro2 >= 0) {
-						double walllength = ((roomcorner2.getX() - roomcorner1.getX())
-								* (roomcorner2.getX() - roomcorner1.getX()))
-								+ ((roomcorner2.getY() - roomcorner1.getY())
-										* (roomcorner2.getY() - roomcorner1.getY())); // checks whether point lies
-																						// between the wall corners
-
-						if (dotpro1 <= walllength && dotpro2 <= walllength) {
-							return true;
-						}
-					}
-
-				}
-
+			Corner corner1 = room.getCorners().get(i);
+			Corner corner2 = room.getCorners().get(k);
+			if (isDoorOnLine(door, corner1, corner2) && isDoorBetween(door, corner1, corner2)) {
+				return true;
 			}
-
 		}
 		return false;
 	}
 
-	static boolean isColinear(Corner roomcorner, Corner doorcorner1, Corner doorcorner2) {
-		double crossZ = roomcorner.getX() * doorcorner1.getY() - roomcorner.getY() * doorcorner1.getX();
-		double crossY = roomcorner.getZ() * doorcorner1.getX() - roomcorner.getX() * doorcorner1.getZ();
-		double crossX = roomcorner.getY() * doorcorner1.getZ() - roomcorner.getZ() * doorcorner1.getY();
+	private static boolean isDoorBetween(Door door, Corner corner1, Corner corner2) {
+		Corner doorcorner1 = door.getCorners().get(0);
+		Corner doorcorner2 = door.getCorners().get(1);
+		double x1 = corner1.getX(), x2 = corner2.getX(), y2 = corner2.getY(), y1 = corner1.getY();
+		double dotpro1 = (doorcorner1.getX() - x1) * (x2 - x1) + (doorcorner1.getY() - y1) * (y2 - y1);
+		double dotpro2 = (doorcorner2.getX() - x1) * (x2 - x1) + (doorcorner2.getY() - y1) * (y2 - y1);
+		double walllength = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+		return dotpro1 >= 0 && dotpro2 >= 0 && dotpro1 <= walllength && dotpro2 <= walllength;
+	}
 
-		double crossZ2 = crossX * doorcorner2.getY() - crossY * doorcorner2.getX();
-		double crossY2 = crossZ * doorcorner2.getX() - crossX * doorcorner2.getZ();
-		double crossX2 = crossY * doorcorner2.getZ() - crossZ * doorcorner2.getY();
-		return crossX2 == 0 && crossY2 == 0 && crossZ2 == 0;
+	static boolean isDoorOnLine(Door door, Corner corner1, Corner corner2) {
+		return isColinear(door.getCorners().get(0), corner1, corner2) && isColinear(door.getCorners().get(1), corner1, corner2);
+	}
+
+	static boolean isColinear(Corner corner1, Corner corner2, Corner corner3) {
+		double x1 = corner1.getX(), y1= corner1.getY(), x2 = corner2.getX(), y2 = corner2.getY(), x3 = corner3.getX(), y3 = corner3.getY();
+		return x1 * (y3-y2) + x2 * (y1-y3) + x3 * (y2-y1) == 0;
 	}
 
 }
--- src/test/java/org/opensourcebim/levelout/util/SpatialAnalysisTests.java
+++ src/test/java/org/opensourcebim/levelout/util/SpatialAnalysisTests.java
@@ -5,57 +5,106 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opensourcebim.levelout.intermediatemodel.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SpatialAnalysisTests {
-	private final Corner corner1 = new Corner(0, 0, 0);
-	private final Corner corner2 = new Corner(6, 0, 0);
 	private Room room1;
 	private Room room2;
-	private Door door;
+	private Door door1;
+	private Door door2;
+	private Door door3;
+	private Door door4;
+	private Door door5;
+	private Door door6;
 
 	@Before
 	public void setupModel() {
+		/*
+		   2      4
+		+--1------+--+
+		|         |   \
+		|         3    6
+		|         |     \
+		+---------+5-----+
+
+		*/
 		room1 = new Room(
 				Arrays.asList(new Corner(0, 0, 0), new Corner(6, 0, 0), new Corner(6, 6, 0), new Corner(0, 6, 0)));
 		room2 = new Room(
-				Arrays.asList(new Corner(6, 0, 0), new Corner(10, 0, 0), new Corner(10, 6, 0), new Corner(6, 6, 0)));
-		door = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 2, 0)));
+				Arrays.asList(new Corner(6, 0, 0), new Corner(10, 0, 0), new Corner(7, 6, 0), new Corner(6, 6, 0)));
+		door1 = new Door(Arrays.asList(new Corner(1, 6, 0), new Corner(2, 6, 0)));
+		door2 = new Door(Arrays.asList(new Corner(1, 7, 0), new Corner(2, 7, 0)));
+		door3 = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 2, 0)));
+		door4 = new Door(Arrays.asList(new Corner(6, 5, 0), new Corner(6, 7, 0)));
+		door5 = new Door(Arrays.asList(new Corner(6, 0, 0), new Corner(7, 0, 0)));
+		door6 = new Door(Arrays.asList(new Corner(9, 2, 0), new Corner(8, 4, 0)));
 	}
 
 	@Test
 	public void testDoorinRoom() {
-		Door door = new Door(Arrays.asList(new Corner(1, 6, 0), new Corner(2, 6, 0)));
-		Door door2 = new Door(Arrays.asList(new Corner(1, 7, 0), new Corner(2, 7, 0)));
-		Door door3 = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 2, 0)));
-		Door door5 = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 11, 0)));
-		Door door6 = new Door(Arrays.asList(new Corner(6, 0, 0), new Corner(6, 1, 0)));
-		Assert.assertTrue(SpatialAnalysis.isDoorInRoom(room1, door));
+		Assert.assertTrue(SpatialAnalysis.isDoorInRoom(room1, door1));
 		Assert.assertFalse(SpatialAnalysis.isDoorInRoom(room1, door2));
 		Assert.assertTrue(SpatialAnalysis.isDoorInRoom(room2, door3));
-		Assert.assertFalse(SpatialAnalysis.isDoorInRoom(room2, door5));
+		Assert.assertFalse(SpatialAnalysis.isDoorInRoom(room2, door4));
+		Assert.assertTrue(SpatialAnalysis.isDoorInRoom(room2, door5));
+		Assert.assertFalse(SpatialAnalysis.isDoorInRoom(room1, door5));
 		Assert.assertTrue(SpatialAnalysis.isDoorInRoom(room2, door6));
 	}
 
 	@Test
 	public void testanalyzeRooms() {
-		Door door = new Door(Arrays.asList(new Corner(1, 6, 0), new Corner(2, 6, 0)));
-		Door door2 = new Door(Arrays.asList(new Corner(1, 7, 0), new Corner(2, 7, 0)));
-		Door door3 = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 2, 0)));
-		Door door5 = new Door(Arrays.asList(new Corner(6, 1, 0), new Corner(6, 11, 0)));
-		Door door6 = new Door(Arrays.asList(new Corner(6, 0, 0), new Corner(6, 1, 0)));
-		List<Door> doors = Arrays.asList(door, door2, door3, door5, door6);
+		List<Door> doors = Arrays.asList(door1, door2, door3, door4, door5, door6);
 		List<Room> rooms = Arrays.asList(room1, room2);
-		Map<Door, List<Room>> receivedresult = SpatialAnalysis.analyzeRooms(rooms, doors);
-		Map<Door, List<Room>> expectedresult = new HashMap<>();
-		expectedresult.put(door6, Arrays.asList(room1, room2));
-		expectedresult.put(door3, Arrays.asList(room1, room2));
-		expectedresult.put(door, Arrays.asList(room1));
-
-		Assert.assertEquals(expectedresult, receivedresult);
+		Map<Door, List<Room>> actualResult = SpatialAnalysis.analyzeRooms(rooms, doors);
+		Map<Door, List<Room>> expectedResult = new HashMap<>();
+		expectedResult.put(door6, Collections.singletonList(room2));
+		expectedResult.put(door5, Collections.singletonList(room2));
+		expectedResult.put(door3, Arrays.asList(room1, room2));
+		expectedResult.put(door1, Collections.singletonList(room1));
+		Assert.assertEquals(expectedResult, actualResult);
 	}
 
+	@Test
+	public void testColinear() {
+		Assert.assertTrue(
+			SpatialAnalysis.isColinear(new Corner(0, 0, 0), new Corner(6, 0, 0), new Corner(2, 0, 0)));
+	}
+
+	@Test
+	public void testNotColinear() {
+		Assert.assertFalse(
+			SpatialAnalysis.isColinear(new Corner(0, 0, 0), new Corner(6, 0, 0), new Corner(2, 6, 0)));
+	}
+
+	@Test
+	public void testColinearSlanted(){
+		Assert.assertTrue(SpatialAnalysis.isColinear( new Corner(10,0,0), new Corner(9,2,0), new Corner(8,4,0)));
+	}
+
+	@Test
+	public void testExternalDoor() {
+		Map<Door, List<Room>> result = SpatialAnalysis.analyzeRooms(List.of(room1), List.of(door3));
+		Assert.assertEquals(1, result.get(door3).size());
+	}
+
+	@Test
+	public void testInternalDoor() {
+		Map<Door, List<Room>> result = SpatialAnalysis.analyzeRooms(List.of(room1, room2), List.of(door3));
+		Assert.assertEquals(2, result.get(door3).size());
+	}
+
+	@Test
+	public void testDoorOnSegment() {
+		Assert.assertTrue(SpatialAnalysis.isDoorOnLine(door1, new Corner(6, 6, 0), new Corner(0, 6, 0)));
+	}
+
+	@Test
+	public void testDoorParallelNotOnSegment() {
+		Assert.assertFalse(SpatialAnalysis.isDoorOnLine(door2, new Corner(6, 6, 0), new Corner(0, 6, 0)));
+	}
+
+	@Test
+	public void testDoorSlantedSegment(){
+		Assert.assertTrue(SpatialAnalysis.isDoorOnLine(door6, new Corner(10, 0, 0), new Corner(7, 6, 0)));
+	}
 }
