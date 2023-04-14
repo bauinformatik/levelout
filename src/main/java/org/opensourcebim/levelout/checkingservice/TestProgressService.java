639c1d3a1668f3fa713788fcf395a097f7dc74ba
hlg <public.void@gmx.de>
2023-04-15 00:16:21 +0200

    test progress service: write dummy extended data

--- src/main/java/org/opensourcebim/levelout/checkingservice/TestProgressService.java
+++ src/main/java/org/opensourcebim/levelout/checkingservice/TestProgressService.java
@@ -1,11 +1,17 @@
 package org.opensourcebim.levelout.checkingservice;
 
 import org.bimserver.interfaces.objects.SObjectType;
-import org.bimserver.models.store.ServiceDescriptor;
-import org.bimserver.plugins.services.AbstractService;
+import org.bimserver.plugins.SchemaName;
+import org.bimserver.plugins.services.AbstractAddExtendedDataService;
 import org.bimserver.plugins.services.BimServerClientInterface;
 
-public class TestProgressService extends AbstractService {
+import java.nio.charset.StandardCharsets;
+
+public class TestProgressService extends AbstractAddExtendedDataService {
+	public TestProgressService() {
+		super(SchemaName.UNSTRUCTURED_UTF8_TEXT_1_0.name());
+	}
+
 	@Override
 	public void newRevision(RunningService runningService, BimServerClientInterface bimServerClientInterface, long poid, long roid, String userToken, long soid, SObjectType settings) throws Exception {
 		runningService.updateProgress(0);
@@ -13,10 +19,7 @@ public class TestProgressService extends AbstractService {
 			Thread.sleep(100);
 			runningService.updateProgress(i);
 		}
+		addExtendedData("This is just a test service. It does not actually check anything.\nBut if you see this, you know that some things work at least.".getBytes(StandardCharsets.UTF_8), "result.txt", "Service execution result", "text/plain", bimServerClientInterface, roid);
 	}
 
-	@Override
-	public void addRequiredRights(ServiceDescriptor serviceDescriptor) {
-		// none required
-	}
 }
